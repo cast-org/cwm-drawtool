dc761cc26e4695a0dc82ea84452034bf14ab7c0e
bgoldowsky@gmail.com <bgoldowsky@gmail.com@6905fabb-e725-289a-1285-1e582edd88cf>
2011-08-26 18:32:38 +0000

    [maven-release-plugin]  copy for tag cwm-drawtool-0.8

---
+++ tags/cwm-drawtool-0.8/src/main/java/org/cast/cwm/drawtool/extension/DrawingStartersExtension.java
@@ -0,0 +1,26 @@
+package org.cast.cwm.drawtool.extension;
+
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.markup.html.panel.Panel;
+
+
+/**
+ * TODO: Make this.
+ * @author jbrookover
+ *
+ */
+public class DrawingStartersExtension extends Panel implements Extension {
+
+	private static final long serialVersionUID = 1L;
+
+	public DrawingStartersExtension(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResourceReference getJavascriptResource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ tags/cwm-drawtool-0.8/src/main/java/org/cast/cwm/drawtool/extension/UploadExtension.java
@@ -0,0 +1,66 @@
+package org.cast.cwm.drawtool.extension;
+
+import org.apache.wicket.Resource;
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.upload.FileUpload;
+import org.apache.wicket.markup.html.form.upload.FileUploadField;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.html.resources.JavascriptResourceReference;
+import org.cast.cwm.drawtool.SvgEditor;
+
+public abstract class UploadExtension extends Panel implements Extension {
+
+	public UploadExtension() {
+		super(SvgEditor.EXTENSION_MARKUP_ID);
+		
+		Form<Void> form = new Form<Void>("form");
+		
+		final FileUploadField file = new FileUploadField("file");
+		form.add(file);
+		
+		form.add(new AjaxButton("submit") {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+				String url = processFile(file.getFileUpload());
+				target.appendJavascript("$('#upload_dialog').hide();");
+				if (url != null)
+					target.appendJavascript("CastTabs.addImage('" + url + "');");
+			}
+		});
+		
+		add(form);
+	}
+
+	private static final long serialVersionUID = 1L;
+	private static final ResourceReference JS = new JavascriptResourceReference(UploadExtension.class, "upload.js") {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		protected Resource newResource() {
+			// TODO: Drop this when we stop testing
+			return super.newResource().setCacheable(false);
+		}
+	};
+	
+	public ResourceReference getJavascriptResource() {
+		return JS;
+	}
+	
+	/**
+	 * Process the uploaded file and return a string that can be used as a URL
+	 * to that file.
+	 * 
+	 * @param file
+	 * @return
+	 */
+	protected abstract String processFile(FileUpload file);
+	
+	
+}
